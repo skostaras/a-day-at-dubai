81c94f0034382380440d9aa9287871bffce6aa82
stefanos.kostaras <stefanos.kostaras@metis.tech>
2021-12-24 14:21:18 +0200

    unified modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
-import { HomeModule } from './components/home.module';
 import { ExamplesModule } from './examples/examples.module';
 
 import { AppComponent } from './app.component';
@@ -14,13 +13,33 @@ import { AuthInterceptor } from './services/auth.interceptor';
 import { ErrorInterceptor } from './services/error.interceptor';
 import { NotificationComponent } from './components/notification/notification.component';
 import { AppleMapsModule } from 'ngx-apple-maps';
-import { OverviewModule } from './components/landmark-overview/overview.module';
+import { CommonModule } from '@angular/common';
+import { NouisliderModule } from 'ng2-nouislider';
+import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
+import { LandmarkOverviewComponent } from './components/landmark-overview/landmark-overview.component';
+import { HomeComponent } from './components/home.component';
+import { BasicelementsComponent } from './components/basicelements/basicelements.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TypographyComponent } from './components/typography/typography.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+import { LoginComponent } from './components/login/login.component';
+import { AllLandmarksComponent } from './components/all-landmarks/all-landmarks.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
         NotificationComponent,
+        LandmarkOverviewComponent,
+        HomeComponent,
+        BasicelementsComponent,
+        NavigationComponent,
+        TypographyComponent,
+        NucleoiconsComponent,
+        LoginComponent,
+        AllLandmarksComponent,
+        FooterComponent
     ],
 
     imports: [
@@ -30,11 +49,12 @@ import { OverviewModule } from './components/landmark-overview/overview.module';
         ReactiveFormsModule,
         RouterModule,
         AppRoutingModule,
-        HomeModule,
         ExamplesModule,
         HttpClientModule,
         AppleMapsModule,
-        OverviewModule
+        CommonModule,
+        NouisliderModule,
+        JwBootstrapSwitchNg2Module,
     ],
 
     providers: [
