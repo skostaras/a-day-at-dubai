6f65828877862031c3c280a7bd987a0ba7745cbe
Stefanos Kostaras <stefanoskostaras@hotmail.com>
2021-12-28 11:48:44 +0200

    home refactor

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { LandmarkWithPhotos } from '../models/landmark-with-photos';
 import { Observable, of } from 'rxjs';
@@ -8,61 +6,22 @@ import { Observable, of } from 'rxjs';
 @Component({
     selector: 'app-components',
     templateUrl: './home.component.html',
-    styles: [`
-    ngb-progressbar {
-        margin-top: 5rem;
-    }
-    `]
 })
 
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
 
-    constructor(config: NgbAccordionConfig,
+    constructor(
         private httpService: HttpService
-    ) {
-        config.closeOthers = true;
-        config.type = 'info';
-    }
-
-    page = 4;
-    page1 = 5;
-    page2 = 3;
-    focus;
-    focus1;
-    focus2;
+    ) { }
 
     allLandmarks$: Observable<LandmarkWithPhotos[]>;
 
-    model: NgbDateStruct;
-
-    public isCollapsed = true;
-    public isCollapsed1 = true;
-    public isCollapsed2 = true;
-
-    state_icon_primary = true;
-
-    isDisabled(date: NgbDateStruct, current: { month: number }) {
-        return date.month !== current.month;
-    }
-
     ngOnInit() {
-        var navbar = document.getElementsByTagName('nav')[0];
-        navbar.classList.add('navbar-transparent');
-        var body = document.getElementsByTagName('body')[0];
-        body.classList.add('index-page');
-
         this.httpService.getAllLandmarks().subscribe(
             landmarks => {
                 this.allLandmarks$ = of(landmarks);
             }
         );
 
-    }
-
-    ngOnDestroy() {
-        var navbar = document.getElementsByTagName('nav')[0];
-        navbar.classList.remove('navbar-transparent');
-        var body = document.getElementsByTagName('body')[0];
-        body.classList.remove('index-page');
     }
 }
