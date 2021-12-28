759acca7f35f520332c0381c7e28a0e0f17c155d
Stefanos Kostaras <stefanoskostaras@hotmail.com>
2021-12-28 15:20:11 +0200

    WIP overview refactor

--- src/app/components/landmark-overview/landmark-overview.component.ts
+++ src/app/components/landmark-overview/landmark-overview.component.ts
@@ -76,23 +76,17 @@ export class LandmarkOverviewComponent implements OnInit {
 
     if (modalDimension === 'sm' && type === 'modal_mini') {
       this.modalService.open(content, { windowClass: 'modal-mini modal-primary', size: 'sm' }).result.then((result) => {
-        this.closeResult = `Closed with: ${result}`;
       }, (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
     } else if (modalDimension == undefined && type === 'Login') {
       this.modalService.open(content, { windowClass: 'modal-login modal-primary' }).result.then((result) => {
-        this.closeResult = `Closed with: ${result}`;
       }, (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
     } else {
       document.getElementsByClassName('modal-dialog')[0].classList.add('modal-xl');
       this.modalService.open(content).result.then((result) => {
 
-        this.closeResult = `Closed with: ${result}`;
       }, (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
     }
 
